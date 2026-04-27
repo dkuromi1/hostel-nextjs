8586c917a19d569910bf8db30ea7c86c9b1f4ec0
Darryl Kuromi <dkuromi@gmail.com>
2026-04-27 08:33:30 +0200

    changed to woff2 latin subset google font

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,12 @@ import { activeInstance } from "@/instances";
 import { getSiteUrl, metadataBase } from "@/lib/metadata";
 import { externalPreconnectOrigins, propertyConfig } from "@/lib/site-data";
 import { cn } from "@/lib/utils";
-import localFont from "next/font/local";
+import { Nunito } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
 
-const nunito = localFont({
-  src: "./fonts/Nunito-VariableFont_wght.ttf",
+const nunito = Nunito({
+  subsets: ["latin"],
   display: "swap",
   variable: "--font-nunito",
 });
